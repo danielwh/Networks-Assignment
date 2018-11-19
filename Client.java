76e14a18d60ac3d1a1b17922ded0433722bcbc2d
danielwh <danielwh@ntlworld.com>
2018-11-19 21:50:37 +0000

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.nio.channels.ClosedChannelException;
 
 public class Client {
 	
@@ -14,9 +17,22 @@ public class Client {
 	public static void main(String[] args) {
 		try 
 		{
+			//On start up, create a client object and communicate with the server
 			Client client = new Client();
 			client.getSongs();
 		} 
+		catch (ClosedChannelException e)
+		{
+			System.out.println("Sorry, the server is currently busy or closed.");
+		}
+		catch (SocketException e)
+		{
+			System.out.println("Sorry, there was a problem accessing the socket.");
+		}
+		catch (InterruptedIOException e)
+		{
+			System.out.println("Sorry, something has interrupted the connection.");
+		}
 		catch (IOException e) 
 		{
 			System.out.println("Sorry, an error has occured.");
@@ -25,6 +41,7 @@ public class Client {
 	
 	public Client() throws IOException
 	{
+		//Attempt to make a connection to the server on port 22222
 		clientSocket = new Socket("localhost",22222);
 		userIn = new BufferedReader(new InputStreamReader(System.in));
 		serverIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -34,19 +51,28 @@ public class Client {
 	
 	public void getSongs() throws IOException
 	{
+		//Prompt the user to enter an artist name
 		System.out.println("Please enter the artist you are interested in:");
 		String artist = userIn.readLine();
+		//Send the name to the server
 		out.println(artist);
+		//Receive the list of songs from the server
 		String songs = serverIn.readLine();
+		//Print the list of songs to the user
 		System.out.println(songs);
 		String quit = "";
 		do
 		{
+			//Prompt the user to close the connection until they do so
 			System.out.println("Please enter 'quit' to disconnect from the server.");
 			quit = userIn.readLine();
 		}while(!quit.equalsIgnoreCase("quit"));
-		clientSocket.close();
-		System.out.println("Connection terminated.");
+		//Send the message to the server
+		out.println(quit);
+		if (clientSocket.isClosed())
+		{
+			System.out.println("Connection terminated.");
+		}
 	}
 
 }
--- Server.java
+++ Server.java
@@ -20,6 +20,7 @@ public class Server {
 	public static void main(String[] args) {
 		try
 		{
+			//On startup, create a server object and wait for a connection request from a client
 			Server server = new Server();
 			server.listen();
 		}
@@ -33,6 +34,7 @@ public class Server {
 	{
 		songs = new HashMap<>();
 		file = new File("Worst100");
+		//Set up a server socket on port 22222
 		serverSocket = new ServerSocket(22222);
 		Scanner sc = new Scanner(file);
 		while (sc.findWithinHorizon("-",0) != null)
@@ -46,23 +48,29 @@ public class Server {
 	public void listen() throws IOException
 	{
 		String artist;
+		//Wait for a connection request from a client and accept it
 		clientSocket = serverSocket.accept();
 		out = new PrintWriter(clientSocket.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		//Receive artist name from the client
 		artist = in.readLine();
 		if (!songs.containsKey(artist))
-		{		
+		{
+			//Return error message if the artists is not contained in the map
 			out.println("Sorry, this artist is not contained in our records.");
 		}
 		else
 		{
+			//Else return the list of songs held in the map
 			out.println(songs.get(artist));
 		}
 		String quit = "";
 		do
 		{
+			//Wait for message to close connection from client
 			quit = in.readLine();
 		}while(!quit.equalsIgnoreCase("quit"));
+		//Close both sockets
 		clientSocket.close();
 		serverSocket.close();
 	}
