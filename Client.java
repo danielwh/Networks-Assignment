61fd1b71792264e743fde67d712d21fdb6164f12
danielwh <danielwh@ntlworld.com>
2018-11-25 18:32:31 +0000

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
@@ -6,6 +8,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
 import java.nio.channels.ClosedChannelException;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 public class Client {
 	
@@ -13,6 +17,11 @@ public class Client {
 	private BufferedReader userIn;
 	private BufferedReader serverIn;
 	private PrintWriter out;
+	private FileWriter writer;
+	private DateTimeFormatter formatter;
+	private Instant instant;
+	private Instant instant1;
+	private File file;
 
 	public static void main(String[] args) {
 		try 
@@ -41,8 +50,12 @@ public class Client {
 	
 	public Client() throws IOException
 	{
+		file = new File("client.log");
+		writer = new FileWriter(file);
 		//Attempt to make a connection to the server on port 22222
 		clientSocket = new Socket("localhost",22222);
+		instant = Instant.now();
+		writer.write("Connection to server established: " + formatter.format(instant));
 		userIn = new BufferedReader(new InputStreamReader(System.in));
 		serverIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		out = new PrintWriter(clientSocket.getOutputStream());
@@ -56,8 +69,11 @@ public class Client {
 		String artist = userIn.readLine();
 		//Send the name to the server
 		out.println(artist);
+		instant = Instant.now();
 		//Receive the list of songs from the server
 		String songs = serverIn.readLine();
+		instant1 = Instant.now();
+		writer.write("\nServer response received: " + formatter.format(instant1) + "\nResponse length: " + songs.getBytes().length + " bytes\nResponse time: " + (instant1.toEpochMilli()-instant.toEpochMilli()) + " ms");
 		//Print the list of songs to the user
 		System.out.println(songs);
 		String quit = "";
@@ -74,5 +90,4 @@ public class Client {
 			System.out.println("Connection terminated.");
 		}
 	}
-
 }
--- Server.java
+++ Server.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -12,10 +15,15 @@ public class Server {
 	
 	private HashMap<String,String> songs;
 	private File file;
+	private File logFile;
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
 	private PrintWriter out;
 	private BufferedReader in;
+	private FileWriter writer;
+	private DateTimeFormatter formatter;
+	private Instant instant;
+	private Instant instant1;
 
 	public static void main(String[] args) {
 		try
@@ -34,46 +42,61 @@ public class Server {
 	{
 		songs = new HashMap<>();
 		file = new File("Worst100");
+		formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		logFile = new File("server.log");
+		writer = new FileWriter(logFile);
 		//Set up a server socket on port 22222
 		serverSocket = new ServerSocket(22222);
+		instant = Instant.now();
+		writer.write("\nServer started: " + formatter.format(instant));
 		Scanner sc = new Scanner(file);
 		while (sc.findWithinHorizon("-",0) != null)
 		{
 			String line = sc.nextLine();
 			Scanner scLine = new Scanner(line);
-		}
-		
+		}		
 	}
 	
 	public void listen() throws IOException
 	{
-		String artist;
-		//Wait for a connection request from a client and accept it
-		clientSocket = serverSocket.accept();
-		out = new PrintWriter(clientSocket.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		//Receive artist name from the client
-		artist = in.readLine();
-		if (!songs.containsKey(artist))
+		try
 		{
-			//Return error message if the artists is not contained in the map
-			out.println("Sorry, this artist is not contained in our records.");
+			String artist;
+			//Wait for a connection request from a client and accept it
+			clientSocket = serverSocket.accept();
+			instant1 = Instant.now();
+			writer.write("\nSuccessful connection to client: " + formatter.format(instant1));
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			//Receive artist name from the client
+			artist = in.readLine();
+			writer.write("\nArtist name received: " + artist);
+			if (!songs.containsKey(artist))
+			{
+				//Return error message if the artists is not contained in the map
+				out.println("Sorry, this artist is not contained in our records.");
+			}
+			else
+			{
+				//Else return the list of songs held in the map
+				out.println(songs.get(artist));
+			}
+			String quit = "";
+			do
+			{
+				//Wait for message to close connection from client
+				quit = in.readLine();
+			}while(!quit.equalsIgnoreCase("quit"));
+			//Close the connection
+			clientSocket.close();
+			instant = Instant.now();
+			long period = instant.toEpochMilli()-instant1.toEpochMilli();
+			writer.write("\nConnection to client terminated: " + formatter.format(instant) + "\nConnection time: " + period + " ms (~" + period/1000 + " seconds)");
 		}
-		else
+		catch(IOException e)
 		{
-			//Else return the list of songs held in the map
-			out.println(songs.get(artist));
+			instant = Instant.now();
+			writer.write("\nConnection error: " + formatter.format(instant));
 		}
-		String quit = "";
-		do
-		{
-			//Wait for message to close connection from client
-			quit = in.readLine();
-		}while(!quit.equalsIgnoreCase("quit"));
-		//Close both sockets
-		clientSocket.close();
-		serverSocket.close();
 	}
-	
-
 }
